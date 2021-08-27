4c3278506a0226f1c1a544813981ac5bd6028e53
dave-tigre <tigrerosd@gmail.com>
2021-08-27 16:07:12 -0400

    init template of site using Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://dave-tigre.github.io/shpe_gpc_react",
     title: "SHPE GPC",
+    pathPrefix: "/shpe_gpc_react"
   },
   plugins: [],
 };

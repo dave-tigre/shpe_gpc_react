bb31fc212215e6357140bc308eb6b948432a5528
David Tigreros <tigrerosd@gmail.com>
2021-09-01 20:06:25 -0400

    updated to include several packages and plugins including fontawesome, react-bootstrap, gatsby mdx plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,29 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `board`,
+        path: `${__dirname}/board`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
   ],
 };

5207235b8c427722cd9e06901ba729c5d2611a23
David Tigreros <tigrerosd@gmail.com>
2021-09-01 20:09:15 -0400

    added fontawesome util

---
+++ src/utils/font-awesome.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faLinkedin)
+library.add(faEnvelope)

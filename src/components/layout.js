63fef483545cae4f3e138c6523504bfd2178bded
David Tigreros <tigrerosd@gmail.com>
2021-09-01 20:07:08 -0400

    updates to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
-import Navbar from './Navbar/Navbar'
-import { Box, Container } from 'react-layout-components';
+import BootstrapNavbar from './Navbar/navbar'
+import { Container } from 'react-layout-components';
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  gatsbyRespImageImage,
 } from './layout.module.css'
 
 const pathPrefix = "shpe_gpc_react/"
@@ -21,11 +22,19 @@ const Layout = ({ pageTitle, children }) => {
       }
     }
   `)
-  console.log(data)
+
   return (
+    
+    
     <Container>
-      <Navbar />
+      <BootstrapNavbar />
       <div className={container}>
+      <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+  crossorigin="anonymous"
+/>
         <title>{pageTitle} | {data.site.siteMetadata.title}</title>
         <main>
           {/* <h1 className={heading}>{pageTitle}</h1> */}

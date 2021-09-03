f3e5877af193f080c088f4c768e8a82ec40fbe24
David Tigreros <tigrerosd@gmail.com>
2021-09-03 08:07:53 -0400

    updated navbar to use gatsby Link and updated dropdown text to be white

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,10 @@ import { Container } from 'react-layout-components';
 import React, { Component } from 'react'; 
 import { Navbar, Nav, NavDropdown, Dropdown } from 'react-bootstrap';
 import Image from "react-bootstrap/Image"
-import './navbar.module.css'
+import {nav,
+        navLink,
+        navDropdown,
+        navDropdownLink} from'./navbar.module.css'
 import { Link } from "gatsby"
 import logoImage from "../../images/shpe_logo2.png"
 
@@ -13,29 +16,30 @@ import logoImage from "../../images/shpe_logo2.png"
 
 const BootstrapNavbar = () => {
   return (
-    
-    <Navbar className ="nav" expand="lg" fixed="top" >
+    <Navbar className ={nav} expand="lg" fixed="top" >
           <Container>
           <Navbar.Brand href="/">
-            <img
-              src={logoImage}
-              width="250"
-              height="40"
-              className="d-inline-block align-top"
-              alt="SHPE Logo"
-            />
+            <Link to="/">
+              <img
+                src={logoImage}
+                width="250"
+                height="40"
+                className="d-inline-block align-top"
+                alt="SHPE Logo"
+              />
+            </Link>
           </Navbar.Brand>
         </Container>
         <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
-            <NavDropdown title="Dropdown" id="nav-dropdown">
-            <NavDropdown.Item href="/history">History</NavDropdown.Item>
-            <NavDropdown.Item href="/board_of_directors">Board of Directors</NavDropdown.Item>
-            <NavDropdown.Item href="/our_mission">Our Mission</NavDropdown.Item>
+            <Link to="/" className={navLink}>Home</Link>
+            <Link to="/blog" className={navLink}>Blog</Link>
+            <NavDropdown title={<span className={navDropdown}>About Us</span>} id="nav-dropdown" menuVariant="light">
+            <NavDropdown.Item ><Link to="/history" className={navDropdownLink}>History</Link></NavDropdown.Item>
+            <NavDropdown.Item><Link to="/board_of_directors" className={navDropdownLink}>Board Of Directors</Link></NavDropdown.Item>
+            <NavDropdown.Item><Link to="/our_mission" className={navDropdownLink}>Our Mission</Link></NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/membership">Membership</Nav.Link>
-            <Nav.Link href="/sponsor">Sponsors</Nav.Link>
+            <Link to="/membership" className={navLink}>Membership</Link>
+            <Link to="/sponsor" className={navLink}>Sponsors</Link>
         </Nav>
     </Navbar>
   )

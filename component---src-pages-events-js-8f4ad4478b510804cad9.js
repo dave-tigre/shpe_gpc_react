c960aa3c248a1f48a3b3a45916923da5a0693e6c
David Tigreros <tigrerosd@gmail.com>
2021-09-19 13:36:45 -0400

    Updates

---
+++ component---src-pages-events-js-8f4ad4478b510804cad9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkshpe_gpc=self.webpackChunkshpe_gpc||[]).push([[651],{312:function(e,n,t){t.r(n);var a=t(7294),l=t(5181);n.default=function(e){var n=e.data;return a.createElement(l.Z,{pageTitle:"Blog"},a.createElement("ul",null,n.allFile.nodes.map((function(e){return a.createElement("li",{key:e.name},e.name)}))))}}}]);
+//# sourceMappingURL=component---src-pages-events-js-8f4ad4478b510804cad9.js.map

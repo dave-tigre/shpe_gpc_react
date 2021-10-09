e246f9c1e600b4aa6122ef6fbdc5595a37ed0e13
dave-tigre <tigrerosd@gmail.com>
2021-10-09 16:13:33 -0400

    updated index banner, board of directors loading, and changed our mission name schema

--- src/pages/board-of-directors.js
+++ src/pages/board-of-directors.js
@@ -4,28 +4,44 @@ import { Container } from 'react-layout-components';
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import { graphql } from 'gatsby'
-import BootstrapCard from "../components/card"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../utils/font-awesome';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Card from "react-bootstrap/Card";
 
-
-const board_map = new Map();
-
+const cardTitleStyle = {textAlign:'center'};
 const BoardOfDirectorsPage = ({ data }) => {
-  console.log(data.allMdx.nodes.map);
+  const boardArray = [];
+  data.allMdx.nodes.forEach((node) => {
+    var heroImage = getImage(node.frontmatter.hero_image);
+    var image = (<GatsbyImage image={heroImage} alt={node.frontmatter.hero_image_alt}/>)
+    if (node.frontmatter.image !== ""){
+      image = (<Card.Img src={node.frontmatter.image} fluid={true} alt="Card image" />)
+    }
+    boardArray.push([node, image]);
+  })
+  
   return (
     <Layout pageTitle="Board Of Directors">
       <h1 className="text-center">2021 Board of Directors</h1>
       <Row xs={1} md={3} className="g-4">
         {
-            data.allMdx.nodes.map((node) => (
+            boardArray.map((boardMember) => (
               <Col>
-                <BootstrapCard imageSource = {node.frontmatter.image} memberTitle={node.frontmatter.title} memberName={node.frontmatter.name}>
-                  <Container>
-                    <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" /> 
-                    <a href={node.frontmatter.linkedin} style={{marginLeft: 1 + 'em'}}>{node.frontmatter.name}</a>
-                  </Container>
-                </BootstrapCard>
+                <Card>
+                  <Card.Body>
+                    {boardMember[1]}
+                    <Card.Title style={cardTitleStyle}>{boardMember[0].frontmatter.position}</Card.Title>
+                    <Card.Subtitle style={cardTitleStyle}>{boardMember[0].frontmatter.name}</Card.Subtitle>
+                    <Card.Text></Card.Text>
+                    <Card.Text>
+                      <Container>
+                        <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" /> 
+                        <a href={boardMember[0].frontmatter.linkedin} style={{marginLeft: 1 + 'em'}}>{boardMember[0].frontmatter.name}</a>
+                      </Container>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
               </Col>
             ))
         }
@@ -37,20 +53,27 @@ const BoardOfDirectorsPage = ({ data }) => {
 
 // graphql querys need to be done outside of page component
 export const query = graphql`
-query aQuery {
+query {
   allMdx(
-    filter: {frontmatter: {name: {ne: null}}}
-    sort: {fields: frontmatter___index}
+    filter: {frontmatter: {type: {eq: "board"}}}
+    sort: {fields: frontmatter___index, order: ASC}
   ) {
     nodes {
       frontmatter {
-        image
+        position
+        name
         index
         linkedin
-        name
-        title
+        image
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
       id
       slug
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import BootstrapCarousel  from '../components/carousel'
 import  'bootstrap/dist/css/bootstrap.min.css';
+import anImage from "../images/hhm2021.jpg";
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <BootstrapCarousel carouselHeight={200} imageHeight={200}/>
+      <StaticImage src="../images/hhm2021.jpg" alt="SHPE HHM" />
       <p>
         SHPE-GPC represents the premier organization aimed at increasing the number of Hispanics contributing to the nation’s innovation and technology future. We recognize that in order to realize professional and personal development, Networking is an essential component. Please visit our website for our upcoming events, as well as events hosted by our community partners in the Greater Philadelphia region.
       </p>

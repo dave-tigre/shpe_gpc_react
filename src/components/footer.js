99decc5ce326a9a85182c477491b1866acacd255
dave-tigre <tigrerosd@gmail.com>
2021-10-10 17:53:13 -0400

    created subscription button as an object and updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,13 @@
 import * as React from "react";
-import { Container } from "react-layout-components";
 import { container, footerButton, footer } from "./footer.module.css";
-import Button from "react-bootstrap/Button"
+import SubscribeButton from "./subscription_button";
 
-
-const pathPrefix = "shpe_gpc_react/";
 const Footer = () => {
  
   return (
       <footer id="footer">
-
-        
-        <a href="https://forms.gle/sRWWVXZZgMMjayPZ9" target="_blank" rel="noopener noreferrer">
-        <Button className={footerButton}>
-          Click Here to Subscribe To Our News Letter!
-        </Button>
-          </a>
-        
+        <SubscribeButton/>
       </footer>
-
   );
 };
 
---
+++ src/components/subscription_button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "react-bootstrap/Button"
+
+const SubscribeButton = () => {
+  return (
+    <a href="https://forms.gle/sRWWVXZZgMMjayPZ9" target="_blank" rel="noopener noreferrer">
+        <Button variant="primary">
+          Click Here to Subscribe To Our News Letter!
+        </Button>
+    </a>
+  );
+};
+
+export default SubscribeButton;

70cd291e70138d83cb60d4e51bbe08f8996b421b
dave-tigre <tigrerosd@gmail.com>
2021-10-10 19:57:55 -0400

    adding updates to events

--- eventinfo/MakeEvent.sh
+++ eventinfo/MakeEvent.sh
@@ -1,15 +1,15 @@
 eventName=$1
-
-mkdir $eventName && touch index.mdx 
+folderName="${3}-$eventName"
+mkdir $folderName && touch index.mdx 
 
 echo --- >> index.mdx
 echo type: "\"event\"" >> index.mdx
 echo title: "\"${2}\"" >> index.mdx
 echo date: "\"${3}\"" >> index.mdx
-echo hero_image: "\"\"" >> index.mdx
+echo hero_image: "\"eventImage.png\"" >> index.mdx
 echo hero_image_alt: "\"\"" >> index.mdx
 echo hero_image_credit_text: "\"\"" >> index.mdx
 echo hero_image_credit_link: "\"\"" >> index.mdx
 echo --- >> index.mdx
 
-mv index.mdx ./$eventName
+mv index.mdx ./$folderName

7bd47f3b7a92a36e568488b82ba32b3a1021770b
dave-tigre <tigrerosd@gmail.com>
2021-10-10 19:15:28 -0400

    updates to makeevent helper, added new event folder

--- eventinfo/MakeEvent.sh
+++ eventinfo/MakeEvent.sh
@@ -3,13 +3,13 @@ eventName=$1
 mkdir $eventName && touch index.mdx 
 
 echo --- >> index.mdx
-echo type: "event" >> index.mdx
+echo type: "\"event\"" >> index.mdx
 echo title: "\"${2}\"" >> index.mdx
 echo date: "\"${3}\"" >> index.mdx
-echo hero_image: "hhm2021.jpg" >> index.mdx
-echo hero_image_alt: "SHPE-GPC Hispanic Heritage Month" >> index.mdx
-echo hero_image_credit_text: "Stephanie Gago" >> index.mdx
-echo hero_image_credit_link: "" >> index.mdx
+echo hero_image: "\"\"" >> index.mdx
+echo hero_image_alt: "\"\"" >> index.mdx
+echo hero_image_credit_text: "\"\"" >> index.mdx
+echo hero_image_credit_link: "\"\"" >> index.mdx
 echo --- >> index.mdx
 
 mv index.mdx ./$eventName

07d83d51481c4532893719f8d3982bdc26e98fa9
dave-tigre <tigrerosd@gmail.com>
2021-10-18 22:52:25 -0400

    updated mission page and added pillar images

--- src/pages/our-mission.js
+++ src/pages/our-mission.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const OurMissionPage = () => {
   return (
@@ -32,6 +33,76 @@ const OurMissionPage = () => {
                   <li>Continuous Improvement</li>
               </ul>
         <h1>The Pillars of SHPE</h1>
+        <div className='row'>
+          <div className='col'>
+            <h6>Professional Development</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/professionalDevelopment.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+            <p> Activities geared at the soft skills and/or technical skills breadth requirement of the members' current workforce competencies.</p>
+          </div>
+          <div className='col'>
+          <h6>Leadership Development</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/leadershipDevelopment.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+          <p> Activities geared towards developing leadership skills of members, ensuring a solid foundation of leaders within the executive board and the successful transition of officers.</p>
+          </div>
+          <div className='col'>
+          <h6>Chapter Development</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/chapterDevelopment.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+          <p>Activities promoting recruitment/retention of members, promote a solid foundation for the chapter itself in terms of funding, presence and/or influence in the local community.</p>
+          </div>
+          <div className='col'>
+          <h6>Community & Outreach Service</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/communityOutreach.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+          <p>Activities specifically promoting STEM awareness in the K-12 community and/or supporting the local community.</p>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+          <h6>Student & Professional Chapter Interaction</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/chapterInteraction.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+          <p>Activities promoting professional and local student SHPE chapter interaction and mentorship by the SHPE professional chapter.</p>
+          </div>
+          <div className='col'>
+          <h6>Career Development</h6>
+            <StaticImage
+              alt="Trivia Night"
+              src="../images/pillars-of-shpe/careerDevelopment.png"
+              className="img-fluid"
+              placeholder="blurred"
+              layout="fullWidth"
+            />
+          <p>Activities geared towards the career and management development with the specific intent of upward mobility and/or advancement within the members' career.</p>
+          </div>
+        </div>
 
     </Layout>
   )

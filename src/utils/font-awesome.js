ad7ac9523820d793faeefe9466117033b16a8ac8
David Tigreros <tigrerosd@gmail.com>
2021-10-19 15:56:00 -0400

    added fontawesome link

--- src/utils/font-awesome.js
+++ src/utils/font-awesome.js
@@ -1,8 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faLinkedin)
 library.add(faInstagram)
 library.add(faFacebook)
 library.add(faEnvelope)
+library.add(faLink)

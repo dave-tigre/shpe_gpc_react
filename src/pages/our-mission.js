1e58b915ea76f1aa02db5116a0833e7ac81fab08
David Tigreros <tigrerosd@gmail.com>
2021-10-20 21:12:25 -0400

    updated list and headers for style and consistency

--- src/pages/our-mission.js
+++ src/pages/our-mission.js
@@ -1,37 +1,45 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Container } from 'react-layout-components'
+import { ListGroup } from 'react-bootstrap'
 
 const OurMissionPage = () => {
   return (
     <Layout pageTitle="Our Mission">
+      <Container style={{textAlign:'center'}}>
           <h1>Mission</h1>
           <p>SHPE's mission is to promote the development of Hispanics in engineering, science and other technical professions to achieve educational excellence, economic opportunity, and social equity. 
               We fulfill this Mission by increasing educational opportunities, promoting professional and personal growth and reinforcing our reputation as a vital Hispanic organization. 
               We also carry out our social responsibility to be involved in education, business and enhancing pride within our organization.</p>
-
-              <u>We are brought together by:</u>
-              <ul>
-                  <li>Heritage</li>
-                  <li>Social responsibility</li>
-                  <li>Desire to improve the equality of all people through the use of science and technology</li>
-              </ul>
-
-              <u>We value excellence in:</u>
-              <ul>
-                  <li>Education</li>
-                  <li>Professional pursuit</li>
-                  <li>Leadership</li>
-              </ul>
-              
-              <u>We obtain excellence through:</u>
-              <ul>
-                  <li>Integrity</li>
-                  <li>Empowerment</li>
-                  <li>Achievement</li>
-                  <li>Diversity</li>
-                  <li>Continuous Improvement</li>
-              </ul>
+              <div className='row' >
+                <div className='col' >
+                  <h5>We are brought together by:</h5>
+                  <ListGroup  variant='flush'>
+                      <ListGroup.Item>Heritage</ListGroup.Item>
+                      <ListGroup.Item>Social responsibility</ListGroup.Item>
+                      <ListGroup.Item>Desire to improve the equality of all people through the use of science and technology</ListGroup.Item>
+                  </ListGroup>
+                </div>
+                <div className='col' >
+                  <h5>We value excellence in:</h5>
+                  <ListGroup variant='flush'>
+                      <ListGroup.Item>Education</ListGroup.Item>
+                      <ListGroup.Item>Professional pursuit</ListGroup.Item>
+                      <ListGroup.Item>Leadership</ListGroup.Item>
+                  </ListGroup>
+                </div>
+                <div className='col' >
+                <h5>We obtain excellence through:</h5>
+                <ListGroup variant='flush'>
+                      <ListGroup.Item>Integrity</ListGroup.Item>
+                      <ListGroup.Item>Empowerment</ListGroup.Item>
+                      <ListGroup.Item>Achievement</ListGroup.Item>
+                      <ListGroup.Item>Diversity</ListGroup.Item>
+                      <ListGroup.Item>Continuous Improvement</ListGroup.Item>
+                  </ListGroup>
+                </div>
+            </div>
         <h1>The Pillars of SHPE</h1>
         
         <div style={{textAlign:'center'}}>
@@ -107,6 +115,7 @@ const OurMissionPage = () => {
           </div>
         </div>
       </div>
+      </Container>
     </Layout>
   )
 }

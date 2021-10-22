4af93804f6a29d0f83d52c258a1963a640b7cdfe
dave-tigre <tigrerosd@gmail.com>
2021-10-22 15:49:36 -0400

    update to banner layout

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -15,54 +15,54 @@ import { Container } from "react-bootstrap";
 
 const BootstrapCarousel = () => {
     return (
-        <Container>
-            <div className={overlay}></div>
+        <Container style={{maxHeight:'500px'}}>
+            <div className={overlay}>
+                <div className={carousel}>
 
-            <div className={carousel}>
-
-                <Carousel>
-                    <Carousel.Item style={
-                        {
-                            textAlign: 'center'
-                        }
-                    }>
-                        <StaticImage 
-                            alt="Philly Skyline" 
-                            src="../images/carousel/philadelphia.png" 
-                            className="img-fluid" 
-                            placeholder="blurred"
-                            height={500}
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item style={
-                        {
-                            textAlign: 'center'
-                        }
-                    }>
-                        <StaticImage 
-                            alt="SHPE National Convention 2018" 
-                            src="../images/carousel/NationalConvention2018.png" 
-                            className="img-fluid" 
-                            placeholder="blurred"
-                            height={500}
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item style={
-                        {
-                            textAlign: 'center'
-                        }
-                    }>
-                        <StaticImage 
-                            alt="Trivia Night" 
-                            src="../images/carousel/Trivia2.png" 
-                            className="img-fluid" 
-                            placeholder="blurred"
-                            height={500}
-                        />
-                    </Carousel.Item>
-                </Carousel>
+                    <Carousel>
+                        <Carousel.Item style={
+                            {
+                                textAlign: 'center'
+                            }
+                        }>
+                            <StaticImage 
+                                alt="Philly Skyline" 
+                                src="../images/carousel/philadelphia.png"
+                                placeholder="blurred"
+                                layout='constrained'
+                                height={500}
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item style={
+                            {
+                                textAlign: 'center'
+                            }
+                        }>
+                            <StaticImage 
+                                alt="SHPE National Convention 2018" 
+                                src="../images/carousel/NationalConvention2018.png" 
+                                placeholder="blurred"
+                                layout='constrained'
+                                height={500}
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item style={
+                            {
+                                textAlign: 'center'
+                            }
+                        }>
+                            <StaticImage 
+                                alt="Trivia Night" 
+                                src="../images/carousel/Trivia2.png"
+                                placeholder="blurred"
+                                layout='constrained'
+                                height={500}
+                            />
+                        </Carousel.Item>
+                    </Carousel>
 
 
+                </div>
             </div>
             <div className={container}>
               <div className={text}>
@@ -70,14 +70,15 @@ const BootstrapCarousel = () => {
                   <h2 className={bodyBanner}>SHPE is the nation’s largest association dedicated to fostering Hispanic leadership in the STEM field.</h2>
                  
                     <div className='row'>
-                        <div className="col-sm-2">
+                        <div className='col-sm-4'>
                             <a href="https://forms.gle/sRWWVXZZgMMjayPZ9" target="_blank" rel="noopener noreferrer">
                                 <Button size="md" className={joinButton}>
                                     Join Us
                                 </Button>
                             </a>
                         </div>
-                        <div className="col-sm-3">
+
+                        <div className='col-sm-6'>
                             <a href="https://www.shpe.org/" target="_blank" rel="noopener noreferrer">
                                 <Button size="md" className={joinButton}>
                                     SHPE National
@@ -87,6 +88,9 @@ const BootstrapCarousel = () => {
                     </div>
                 </div>
             </div>
+
+            
+            
         </Container>
     );
 };
